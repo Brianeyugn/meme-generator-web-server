b339865b84061680fb8fe5a8872a928d0e76094e
cstrain2000 <cstrain2000@g.ucla.edu>
2023-04-23 05:50:26 +0000

    ConfigStatement tests
    Change-Id: Idc3c65280db182cb77cd6bdec760d1c735f9582d

---
+++ tests/config_statement_test.cc
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+#include "config_parser.h"
+
+class NginxConfigStatementTest : public ::testing::Test {
+  protected:
+    void SetUp() override {
+      return;
+    }
+  NginxConfigStatement configStatement;
+};
+
+// ToString() TESTS
+// Test for just newline if depth is zero with no tokens.
+TEST_F(NginxConfigStatementTest, NothingInStatement) {
+  bool success = configStatement.ToString(0) == ";\n";
+  EXPECT_TRUE(success);
+}
+
+// Test for whitespace for depth of 1 with no tokens.
+TEST_F(NginxConfigStatementTest, OneDepth) {
+  bool success = configStatement.ToString(1) == "  ;\n";
+  EXPECT_TRUE(success);
+}
+
+// Test for depth of 2 with 2 tokens.
+TEST_F(NginxConfigStatementTest, TwoTokens) {
+  configStatement.tokens_.push_back("One");
+  configStatement.tokens_.push_back("Two");
+  bool success = configStatement.ToString(2) == "    One Two;\n";
+  EXPECT_TRUE(success);
+}
+
+// Test for depth of 0 with 2 tokens.
+TEST_F(NginxConfigStatementTest, TwoTokensZeroDepth) {
+  configStatement.tokens_.push_back("One");
+  configStatement.tokens_.push_back("Two");
+  bool success = configStatement.ToString(0) == "One Two;\n";
+  EXPECT_TRUE(success);
+}
